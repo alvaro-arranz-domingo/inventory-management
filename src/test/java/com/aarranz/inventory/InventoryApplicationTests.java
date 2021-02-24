017332d29b46b0cfe3d2a8125b4c56879e089cbb
Alvaro Arranz <alvaro.arranz.domingo@gmail.com>
2021-02-24 16:38:48 +0100

    Project initial setup

---
+++ src/test/java/com/aarranz/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarranz.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
