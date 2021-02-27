35cc008e36dc8eb2cd982759e2e980b0b23ad19a
Alvaro Arranz <alvaro.arranz.domingo@gmail.com>
2021-02-27 14:57:19 +0100

    Added swagger documentation at /swagger-ui/

--- src/main/java/com/aarranz/inventory/AppConfiguration.java
+++ src/main/java/com/aarranz/inventory/AppConfiguration.java
@@ -7,8 +7,14 @@ import com.aarranz.inventory.core.usecases.GetAllProductsUC;
 import com.aarranz.inventory.core.usecases.SellProductUC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class AppConfiguration {
 
   @Bean
@@ -21,5 +27,14 @@ public class AppConfiguration {
                                      SellRepository sells,
                                      TransactionProvider transactionProvider) {
     return new SellProductUC(products, sells, transactionProvider);
+  }
+
+  @Bean
+  public Docket apiDocket() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.aarranz.inventory.infrastructure.rest"))
+        .paths(PathSelectors.any())
+        .build();
   }
 }
